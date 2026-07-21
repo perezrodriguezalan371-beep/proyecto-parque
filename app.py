75c64b7617aa5fd389b4c4743399c25702f8d949
Alan Jared Pérez Rodríguez <perezrodriguezalan371@gmail.com>
2026-07-21 17:10:57 -0600

    Corrección de rutas e imágenes en plantillas

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def inicio():
     return render_template('index.html')
 
+@app.route('/historia')
+def historia():
+    return render_template('historia.html')
+
+@app.route('/leyendas')
+def leyendas():
+    return render_template('leyendas.html')
+
+@app.route('/eventos')
+def eventos():
+    return render_template('eventos.html')
+
 if __name__ == '_main_':
     app.run(debug=True)
